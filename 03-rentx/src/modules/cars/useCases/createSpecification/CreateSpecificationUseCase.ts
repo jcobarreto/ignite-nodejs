a39f979dc9dbaa7f8f0e74ff3acb69117fe7500d
jcobarreto <julio.barreto@gmail.com>
2021-06-09 09:35:59 -0300

    Implement error handling

--- 03-rentx/src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ 03-rentx/src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { ISpecificationRepository } from "../../repositories/ISpecificationsRepository";
 
 interface IRequest {
@@ -18,7 +19,7 @@ class CreateSpecificationUseCase {
       await this.specificationsRepository.findByName(name);
 
     if (specificationAlreadyExists) {
-      throw new Error("Specifications already exists!");
+      throw new AppError("Specifications already exists!");
     }
 
     await this.specificationsRepository.create({ name, description });
