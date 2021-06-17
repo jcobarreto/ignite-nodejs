302575d9624b00c5b549226e3fea4f49e4295344
jcobarreto <julio.barreto@gmail.com>
2021-06-17 14:49:00 -0300

    Creating user seed

--- 03-rentx/src/shared/infra/http/server.ts
+++ 03-rentx/src/shared/infra/http/server.ts
@@ -3,15 +3,14 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUI from "swagger-ui-express";
 
-import "../typeorm";
-
 import "@shared/container";
-
 import { AppError } from "@shared/errors/AppError";
+import createConnection from "@shared/infra/typeorm";
 
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
 
+createConnection();
 const app = express();
 
 app.use(express.json());
