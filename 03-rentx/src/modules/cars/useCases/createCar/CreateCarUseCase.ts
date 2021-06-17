269a104207d0db08c438548de76f7d9b3e790b3f
jcobarreto <julio.barreto@gmail.com>
2021-06-17 11:08:32 -0300

    Implementing entity cars

--- 03-rentx/src/modules/cars/useCases/createCar/CreateCarUseCase.ts
+++ 03-rentx/src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -14,10 +14,10 @@ interface IRequest {
   category_id: string;
 }
 
-// @injectable()
+@injectable()
 class CreateCarUseCase {
   constructor(
-    //    @inject("CarsRepository")
+    @inject("CarsRepository")
     private carsRepository: ICarsRepository
   ) { }
   async execute({
@@ -29,7 +29,9 @@ class CreateCarUseCase {
     brand,
     category_id,
   }: IRequest): Promise<Car> {
-    const carAlreadyExists = await this.carsRepository.findByLicensePlate(license_plate);
+    const carAlreadyExists = await this.carsRepository.findByLicensePlate(
+      license_plate
+    );
 
     if (carAlreadyExists) {
       throw new AppError("Car already exists");
